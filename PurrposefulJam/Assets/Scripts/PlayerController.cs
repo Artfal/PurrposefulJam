144a69c9fe7c02fad4e4e87a44b7c8608bee9894
Artfal <51421585+Artfal@users.noreply.github.com>
2020-10-11 13:10:00 +0200

    Combined Scripts and Refactored.
    Combined Jump and Controller scripts, cleaned up the code.
    Player cannot control in the air, but still maintains momentum in the air

---
+++ PurrposefulJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+
+    public Rigidbody2D theRB;
+
+    private bool isGrounded;
+    public Transform groundPoint;
+    public LayerMask whatIsGround;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundPoint.position, 0.2f, whatIsGround);
+        Movement();
+        Jump();
+    }
+
+    private void Movement()
+    {
+        if (isGrounded)
+        {
+            theRB.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), theRB.velocity.y);
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+        }
+    }
+}
