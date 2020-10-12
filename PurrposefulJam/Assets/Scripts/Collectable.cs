5ac15e0bb6a6f672909ece321378ff512acdb7d0
Artfal <51421585+Artfal@users.noreply.github.com>
2020-10-12 17:31:18 +0200

    Cleaned up scripts

--- PurrposefulJam/Assets/Scripts/Collectable.cs
+++ PurrposefulJam/Assets/Scripts/Collectable.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
+    public static Collectable instance;
+
+    public int collectable = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,16 @@ public class Collectable : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            collectable++;
+            UIController.instance.UpdateCollectableText();
+            Debug.Log("Player Collected Item");
+            Destroy(gameObject);
+        }
     }
 }
---
+++ PurrposefulJam/Assets/Scripts/UIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public static UIController instance;
+
+    public Text collectableText;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collectableText.text = Collectable.instance.collectable.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UpdateCollectableText()
+    {
+        collectableText.text = Collectable.instance.collectable.ToString();
+    }
+}
