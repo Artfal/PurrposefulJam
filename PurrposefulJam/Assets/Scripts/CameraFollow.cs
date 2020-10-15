aea2d9371e9627f109387bf2f4f294908d18b492
Artfal <51421585+Artfal@users.noreply.github.com>
2020-10-15 20:22:49 +0200

    Added and Updated Art assets

--- PurrposefulJam/Assets/Scripts/CameraFollow.cs
+++ PurrposefulJam/Assets/Scripts/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
-    public float minHeight, maxHeight;
+    public float minHeight, maxHeight, minScroll, maxScroll;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +16,7 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, Mathf.Clamp(player.transform.position.y, minHeight, maxHeight), transform.position.z);
+        //transform.position = new Vector3(player.transform.position.x, Mathf.Clamp(player.transform.position.y, minHeight, maxHeight), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(player.transform.position.x, minScroll, maxScroll), Mathf.Clamp(player.transform.position.y, minHeight, maxHeight), transform.position.z);
     }
 }
